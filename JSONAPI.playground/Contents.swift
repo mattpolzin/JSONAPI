b3a4591a5dcb5a5516b0f6c097a8e42f97cbe390
Mathew Polzin <matt.polzin@gmail.com>
2018-11-18 21:51:22 -0800

    comment out the rest of the once again relevant playground example

--- JSONAPI.playground/Contents.swift
+++ JSONAPI.playground/Contents.swift
@@ -23,18 +23,19 @@ let dogFromData = dogResponse.body.data?.primary.value
 
 // MARK: - Create a request or response with multiple people and dogs and houses included
 let personIds = [Person.Identifier(), Person.Identifier()]
-let dogs = try [Dog(name: "Buddy", owner: personIds[0]), Dog(name: "Joy", owner: personIds[0]), Dog(name: "Travis", owner: personIds[1])]
+let dogs = try! [Dog(name: "Buddy", owner: personIds[0]), Dog(name: "Joy", owner: personIds[0]), Dog(name: "Travis", owner: personIds[1])]
 let houses = [House(), House()]
-let people = try [Person(id: personIds[0], name: ["Gary", "Doe"], favoriteColor: "Orange-Red", friends: [], dogs: [dogs[0], dogs[1]], home: houses[0]), Person(id: personIds[1], name: ["Elise", "Joy"], favoriteColor: "Red", friends: [], dogs: [dogs[2]], home: houses[1])]
+let people = try! [Person(id: personIds[0], name: ["Gary", "Doe"], favoriteColor: "Orange-Red", friends: [], dogs: [dogs[0], dogs[1]], home: houses[0]), Person(id: personIds[1], name: ["Elise", "Joy"], favoriteColor: "Red", friends: [], dogs: [dogs[2]], home: houses[1])]
 
 typealias BatchPeopleDocument = JSONAPIDocument<ManyResourceBody<Person>, Include2<Dog, House>, BasicJSONAPIError>
 
 let includes = dogs.map { BatchPeopleDocument.Include($0) } + houses.map { BatchPeopleDocument.Include($0) }
 let batchPeopleDocument = BatchPeopleDocument(body: .init(entities: people), includes: .init(values: includes))
+let batchPeopleData = try! JSONEncoder().encode(batchPeopleDocument)
 
 // MARK: - Parse a request or response body with multiple people in it and dogs and houses included
 
-//let peopleResponse = try! JSONDecoder().decode(BatchPeopleResponse.self, from: dummyData)
-//let people = peopleResponse.body.data?.primary.values
-//let dogs = peopleResponse.body.data?.included[Dog.self]
-//let houses = peopleResponse.body.data?.included[House.self]
+let peopleResponse = try! JSONDecoder().decode(BatchPeopleDocument.self, from: batchPeopleData)
+let peopleFromData = peopleResponse.body.data?.primary.values
+let dogsFromData = peopleResponse.body.data?.included[Dog.self]
+let housesFromData = peopleResponse.body.data?.included[House.self]
