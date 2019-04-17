b1adc6071922184bafaf1cf03332a189997ecd98
Mathew Polzin <matt.polzin@gmail.com>
2019-04-16 21:52:50 -0700

    Update package and podspec files

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
 			targets: ["JSONAPITesting"])
     ],
     dependencies: [
-		.package(url: "https://github.com/mattpolzin/Poly.git", from: "1.0.0"),
+		.package(url: "https://github.com/mattpolzin/Poly.git", from: "2.0.0"),
     ],
     targets: [
         .target(
