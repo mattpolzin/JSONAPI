4e0e44c45d8f9834f347a04ad80dcad7c72473e2
Mathew Polzin <matt.polzin@gmail.com>
2019-06-12 20:15:07 -0700

    Update README after renaming. Rename test lib entity checker.

--- Sources/JSONAPITesting/ResourceObjectCheck.swift
+++ Sources/JSONAPITesting/ResourceObjectCheck.swift
@@ -1,5 +1,5 @@
 //
-//  EntityCheck.swift
+//  ResourceObjectCheck.swift
 //  JSONAPITesting
 //
 //  Created by Mathew Polzin on 11/27/18.
@@ -7,7 +7,7 @@
 
 import JSONAPI
 
-public enum EntityCheckError: Swift.Error {
+public enum ResourceObjectCheckError: Swift.Error {
 	/// The attributes should live in a struct, not
 	/// another type class.
 	case attributesNotStruct
@@ -29,8 +29,8 @@ public enum EntityCheckError: Swift.Error {
 	case nullArray(named: String)
 }
 
-public struct EntityCheckErrors: Swift.Error {
-	let problems: [EntityCheckError]
+public struct ResourceObjectCheckErrors: Swift.Error {
+	let problems: [ResourceObjectCheckError]
 }
 
 private protocol OptionalAttributeType {}
@@ -56,7 +56,7 @@ extension Attribute: _AttributeType {}
 
 public extension ResourceObject {
 	static func check(_ entity: ResourceObject) throws {
-		var problems = [EntityCheckError]()
+		var problems = [ResourceObjectCheckError]()
 
 		let attributesMirror = Mirror(reflecting: entity.attributes)
 
@@ -88,7 +88,7 @@ public extension ResourceObject {
 		}
 
 		guard problems.count == 0 else {
-			throw EntityCheckErrors(problems: problems)
+			throw ResourceObjectCheckErrors(problems: problems)
 		}
 	}
 }
